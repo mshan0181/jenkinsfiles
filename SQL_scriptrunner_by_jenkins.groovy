99ce188b5a3419d2dfa08f0edb214f7663cae3f1
mshan0181 <103882696+mshan0181@users.noreply.github.com>
2024-01-07 15:31:12 +0530

    first commit for pipeline script

---
+++ SQL_scriptrunner_by_jenkins.groovy
@@ -0,0 +1,6 @@
+node {
+   echo 'SQLPlusRunner running user define script for shandba@client_access'
+   step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'userDefined', script: '',scriptContent: 'select * from v$version'])
+   step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'userDefined', script: '',scriptContent: 'select name from v$database'])
+   step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'file', script: 'switchover_status.sql',scriptContent: ''])
+}

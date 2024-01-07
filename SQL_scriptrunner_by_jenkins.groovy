3f70eab7b9e1581436b7270a2635c31fe7aede40
mshan0181 <103882696+mshan0181@users.noreply.github.com>
2024-01-07 15:48:22 +0530

    2nd commit

--- SQL_scriptrunner_by_jenkins.groovy
+++ SQL_scriptrunner_by_jenkins.groovy
@@ -2,5 +2,4 @@ node {
    echo 'SQLPlusRunner running user define script for shandba@client_access'
    step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'userDefined', script: '',scriptContent: 'select * from v$version'])
    step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'userDefined', script: '',scriptContent: 'select name from v$database'])
-   step([$class: 'SQLPlusRunnerBuilder',credentialsId:'shandb-priv-id', instance:'client_access',scriptType:'file', script: 'switchover_status.sql',scriptContent: ''])
-}
+  }
